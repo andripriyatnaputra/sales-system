ad9ae916d4f22424d5a731bfbdbf7515fbbc51e5
Andri Putra <andripriyatnaputra@gmail.com>
2026-01-06 14:45:42 +0700

    update main.go

--- backend/main.go
+++ backend/main.go
@@ -30,7 +30,10 @@ func main() {
 	// Setup Gin router
 	r := gin.Default()
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins: []string{
+			"http://localhost:3000",
+			"http://202.50.203.136:3000",
+		},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
