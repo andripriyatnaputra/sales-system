768b013c06fc3ae0c7369a10e41389ee3367fe25
Andri Putra <andripriyatnaputra@gmail.com>
2026-01-06 11:22:06 +0700

    update Dockerfile

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,6 +2,7 @@ console.log(">>> Next.js rewrites loaded, backend:8081 <<<");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   async rewrites() {
     return [
       {
