ff74b929fa7d4f34d4655e7275874781bb1ac0d1
Andri Putra <andripriyatnaputra@gmail.com>
2026-01-19 10:51:54 +0700

    Fixed Prj Code

--- backend/handlers/project_create.go
+++ backend/handlers/project_create.go
@@ -74,6 +74,7 @@ func CreateProject(c *gin.Context) {
 	}
 
 	fmt.Println("GENERATED projectCode:", projectCode)
+	fmt.Sprintf("GENERATED projectCode:", projectCode)
 
 	var id int64
 	err = tx.QueryRow(ctx, `
